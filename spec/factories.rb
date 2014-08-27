222644073e84e21a976793e0f9795045a488e879
wjtaylor92 <wjtaylor@ncsu.edu>
2014-08-27 19:47:07 -0400

    Sign Up done

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "William Taylor"
+    email    "wjtaylor@ncsu.edu"
+    password "jackat8214"
+    password_confirmation "jackat8214"
+  end
+end
