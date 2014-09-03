756301473b040a73d180900c8f7f82da18b1390b
wjtaylor92 <wjtaylor@ncsu.edu>
2014-09-03 18:32:00 -0400

    Finished user edit, update, index, etc.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     	else
       	flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       	render 'new'
-    end
+    	end
 	end
 
 	def destroy
